37ee935f1b7cc5869b236be5e29b2db8a8613af5
Emil <bloofyreaper@gmail.com>
2017-09-26 14:53:15 +0300

    add List project

---
+++ List/src/ru/itis/Main.java
@@ -0,0 +1,12 @@
+package ru.itis;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.addToEnd(7);
+        list.addToEnd(8);
+        list.addToEnd(9);
+        list.showList();
+    }
+}
