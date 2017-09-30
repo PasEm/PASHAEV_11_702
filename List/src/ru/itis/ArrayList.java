2e1083f02c8f4351895ae21c6e910014b535cf88
Emil <bloofyreaper@gmail.com>
2017-09-30 23:14:14 +0300

    ArrayList(HomeTask05)

--- List/src/ru/itis/ArrayList.java
+++ List/src/ru/itis/ArrayList.java
@@ -1,19 +1,68 @@
 package ru.itis;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayList {
-    int array[] = new int[10];
-    int count = 0;
-    void addToEnd(int element){
-        if (count< array.length){
-            array[count] = element;
-            count++;
-        } else {
-            System.err.println("Нет места");
+    int array[] = new int[100];
+    int count = 0, firstsize = 0;
+    void addToEnd(){
+        Scanner scanner = new Scanner(System.in);
+        int element = scanner.nextInt();
+        if (count + 1 < array.length){
+                increase();
         }
+        array[count] = element;
+        count++;
     }
     void showList() {
-        for (int i = 0; i < count; i++){
+        for (int i = 0; i < (count > firstsize ? count : firstsize); i++){
             System.out.print(array[i] + " ");
         }
+        System.out.println();
+    }
+    void increase(){
+        array = Arrays.copyOf(array, array.length * 2);
+    }
+    void deleteNumber(){
+        Scanner scanner = new Scanner(System.in);
+        int index = scanner.nextInt();
+        array[index] = 0;
+        for (int i = index; i < count; i++){
+            int z = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = z;
+        }
+        count--;
+    }
+    void menu(){
+        System.out.println("МЕНЮ:");
+        System.out.println("1. Создать массив N размерности");
+        System.out.println("2. Добавить число в конец массива");
+        System.out.println("3. Удалить число из массива");
+        System.out.println("4. Вставить число в массив");
+        System.out.println("5. Вывести массив");
+        System.out.println("6. Выход из меню");
+    }
+    void pushNumber(){
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int index = scanner.nextInt();
+        if (array.length - 1 < index){
+            increase();
+        }
+        count = index > count ? index + 1 : + 1;
+        for (int i = count-1; i >= index; i--){
+            array[i+1] = array[i];
+        }
+        array[index] = number;
+    }
+    void exit(){
+        System.exit(0);
+    }
+    void init(){
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        firstsize = size;
     }
 }
--- List/src/ru/itis/Main.java
+++ List/src/ru/itis/Main.java
@@ -1,12 +1,34 @@
 package ru.itis;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         ArrayList list = new ArrayList();
-        list.addToEnd(7);
-        list.addToEnd(8);
-        list.addToEnd(9);
-        list.showList();
+        Scanner scanner = new Scanner(System.in);
+        int command = 0;
+        while (command != 6){
+            list.menu();
+            command = scanner.nextInt();
+            if (command == 1){
+                list.init();
+            }
+            if (command == 2){
+                list.addToEnd();
+            }
+            if (command == 3){
+                list.deleteNumber();
+            }
+            if (command == 4){
+                list.pushNumber();
+            }
+            if (command == 5){
+                list.showList();
+            }
+            if (command == 6){
+                list.exit();
+            }
+        }
     }
 }
