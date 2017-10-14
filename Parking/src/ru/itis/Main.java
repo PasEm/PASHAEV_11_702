ee295a5816329dd1b40a854675cb0646a44a7aa8
Emil <bloofyreaper@gmail.com>
2017-10-14 15:20:42 +0300

    Parking

--- Parking/src/ru/itis/Main.java
+++ Parking/src/ru/itis/Main.java
@@ -3,7 +3,7 @@ package ru.itis;
 public class Main {
 
     public static void main(String[] args) {
-        ParkingPlace parking1 = new ParkingPlace("B39");
+        ParkingPlace parking1 = new ParkingPlace("B38");
         ParkingPlace parking5 = new ParkingPlace("E45");
         Car car1 = new Car("Porsche", "black", 666);
         Car car5 = new Car("Mercedez", "blue", 777);
